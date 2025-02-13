f2580132f8c4d25f31b838d39bde6eecead1d789
Mohd Adil <90774182+adil088@users.noreply.github.com>
2025-02-14 01:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if(!s1 || !s2) return -1;
+
+	return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
